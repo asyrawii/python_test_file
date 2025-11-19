fd163cd1adae716c33d9d70946f8d1f66cda21fe
Muhammad Asyrawi  Bin Abdullah Zawawi <64827138+asyrawii@users.noreply.github.com>
2025-11-19 17:10:18 +0800

    Update test_add_numbers.py

--- test_add_numbers.py
+++ test_add_numbers.py
@@ -1,3 +1,5 @@
+# Write a Python3 function *add_numbers(x, y)* that returns the sum of two numeric values parameter *x* and *y*.
+
 from exercise_2 import add_numbers
 def test_add_numbers1():
   assert sqr(2,2) == 4
