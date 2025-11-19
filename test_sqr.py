a8f4fca3ec938ff88b307e8c2a1d6c771cfde2bf
Muhammad Asyrawi  Bin Abdullah Zawawi <64827138+asyrawii@users.noreply.github.com>
2025-11-19 17:04:18 +0800

    Update test_sqr.py

--- test_sqr.py
+++ test_sqr.py
@@ -1,3 +1,10 @@
+# Write a Python3 function *sqr(n)* that returns the square of its numeric parameter *n*.
+"""Sample code
+def sqr(n):
+    return n * n
+"""
+
+
 from exercise_1 import sqr
 def test_sqr1():
   assert sqr(2) == 4
