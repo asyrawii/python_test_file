5029b13d039499c5b833c73c3c1660551833cc44
Muhammad Asyrawi  Bin Abdullah Zawawi <64827138+asyrawii@users.noreply.github.com>
2025-11-19 16:59:32 +0800

    Update test_add_numbers.py

--- test_add_numbers.py
+++ test_add_numbers.py
@@ -1,7 +1,7 @@
 from exercise_2 import add_numbers
-def test_sqr1():
-  assert sqr(2) == 4
-def test_sqr2():
-  assert sqr(-2) == 4
-def test_sqr3():
-  assert sqr(0) == 0
+def test_add_numbers1():
+  assert sqr(2,2) == 4
+def test_add_numbers2():
+  assert sqr(-2,-2) == 0
+def test_add_numbers3():
+  assert sqr(0,0) == 0
